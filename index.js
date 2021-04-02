8bd6ed3aaa6da1da8ef8d514c599074c35842800
Singh8248 <36563001+Singh8248@users.noreply.github.com>
2021-04-02 23:14:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h5>Welcome to Docker's Demo!</h5>');
+    res.send('<h5>Welcome to Docker Demo!</h5>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
